7cdaaf4d85c7ad228a826dfd052225dfa1da65d4
davidr-delfosti <152659995+davidr-delfosti@users.noreply.github.com>
2023-12-27 14:24:51 -0500

    qa

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World! HOLA MUNDO!");
+    return res.send("Hello World! QA!");
 })
 
 app.get('/api', function (req, res) {
